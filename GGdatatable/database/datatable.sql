1ee5e1c9bc4719ed58770eb7ef43990551ddb323
gugusd999 <49971455+gugusd999@users.noreply.github.com>
2019-06-15 10:37:31 +0700

    Data Table Example
    contoh database yang bisa digunakan ada di folder database

---
+++ GGdatatable/database/datatable.sql
@@ -0,0 +1,154 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : home
+Source Server Version : 50621
+Source Host           : 127.0.0.1:3306
+Source Database       : datatable
+
+Target Server Type    : MYSQL
+Target Server Version : 50621
+File Encoding         : 65001
+
+Date: 2019-06-15 10:36:04
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for datatable
+-- ----------------------------
+DROP TABLE IF EXISTS `datatable`;
+CREATE TABLE `datatable` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data1` varchar(255) DEFAULT NULL,
+  `data2` varchar(255) DEFAULT NULL,
+  `data3` varchar(255) DEFAULT NULL,
+  `data4` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=122 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of datatable
+-- ----------------------------
+INSERT INTO `datatable` VALUES ('1', 'nilai1', 'nilai1', 'nilai1', 'nilai1');
+INSERT INTO `datatable` VALUES ('2', 'nilai2', 'nilai2', 'nilai2', 'nilai2');
+INSERT INTO `datatable` VALUES ('3', 'nilai3', 'nilai3', 'nilai3', 'nilai3');
+INSERT INTO `datatable` VALUES ('4', 'nilai4', 'nilai4', 'nilai4', 'nilai4');
+INSERT INTO `datatable` VALUES ('5', 'nilai5', 'nilai5', 'nilai5', 'nilai5');
+INSERT INTO `datatable` VALUES ('6', 'nilai6', 'nilai6', 'nilai6', 'nilai6');
+INSERT INTO `datatable` VALUES ('7', 'nilai7', 'nilai7', 'nilai7', 'nilai7');
+INSERT INTO `datatable` VALUES ('8', 'nilai8', 'nilai8', 'nilai8', 'nilai8');
+INSERT INTO `datatable` VALUES ('9', 'nilai9', 'nilai9', 'nilai9', 'nilai9');
+INSERT INTO `datatable` VALUES ('10', 'nilai10', 'nilai10', 'nilai10', 'nilai10');
+INSERT INTO `datatable` VALUES ('11', 'nilai11', 'nilai11', 'nilai11', 'nilai11');
+INSERT INTO `datatable` VALUES ('12', 'nilai12', 'nilai12', 'nilai12', 'nilai12');
+INSERT INTO `datatable` VALUES ('13', 'nilai13', 'nilai13', 'nilai13', 'nilai13');
+INSERT INTO `datatable` VALUES ('14', 'nilai14', 'nilai14', 'nilai14', 'nilai14');
+INSERT INTO `datatable` VALUES ('15', 'nilai15', 'nilai15', 'nilai15', 'nilai15');
+INSERT INTO `datatable` VALUES ('16', 'nilai16', 'nilai16', 'nilai16', 'nilai16');
+INSERT INTO `datatable` VALUES ('17', 'nilai17', 'nilai17', 'nilai17', 'nilai17');
+INSERT INTO `datatable` VALUES ('18', 'nilai18', 'nilai18', 'nilai18', 'nilai18');
+INSERT INTO `datatable` VALUES ('19', 'nilai19', 'nilai19', 'nilai19', 'nilai19');
+INSERT INTO `datatable` VALUES ('20', 'nilai20', 'nilai20', 'nilai20', 'nilai20');
+INSERT INTO `datatable` VALUES ('21', 'nilai21', 'nilai21', 'nilai21', 'nilai21');
+INSERT INTO `datatable` VALUES ('22', 'nilai22', 'nilai22', 'nilai22', 'nilai22');
+INSERT INTO `datatable` VALUES ('23', 'nilai23', 'nilai23', 'nilai23', 'nilai23');
+INSERT INTO `datatable` VALUES ('24', 'nilai24', 'nilai24', 'nilai24', 'nilai24');
+INSERT INTO `datatable` VALUES ('25', 'nilai25', 'nilai25', 'nilai25', 'nilai25');
+INSERT INTO `datatable` VALUES ('26', 'nilai26', 'nilai26', 'nilai26', 'nilai26');
+INSERT INTO `datatable` VALUES ('27', 'nilai27', 'nilai27', 'nilai27', 'nilai27');
+INSERT INTO `datatable` VALUES ('28', 'nilai28', 'nilai28', 'nilai28', 'nilai28');
+INSERT INTO `datatable` VALUES ('29', 'nilai29', 'nilai29', 'nilai29', 'nilai29');
+INSERT INTO `datatable` VALUES ('30', 'nilai30', 'nilai30', 'nilai30', 'nilai30');
+INSERT INTO `datatable` VALUES ('31', 'nilai31', 'nilai31', 'nilai31', 'nilai31');
+INSERT INTO `datatable` VALUES ('32', 'nilai32', 'nilai32', 'nilai32', 'nilai32');
+INSERT INTO `datatable` VALUES ('33', 'nilai33', 'nilai33', 'nilai33', 'nilai33');
+INSERT INTO `datatable` VALUES ('34', 'nilai34', 'nilai34', 'nilai34', 'nilai34');
+INSERT INTO `datatable` VALUES ('35', 'nilai35', 'nilai35', 'nilai35', 'nilai35');
+INSERT INTO `datatable` VALUES ('36', 'nilai36', 'nilai36', 'nilai36', 'nilai36');
+INSERT INTO `datatable` VALUES ('37', 'nilai37', 'nilai37', 'nilai37', 'nilai37');
+INSERT INTO `datatable` VALUES ('38', 'nilai38', 'nilai38', 'nilai38', 'nilai38');
+INSERT INTO `datatable` VALUES ('39', 'nilai39', 'nilai39', 'nilai39', 'nilai39');
+INSERT INTO `datatable` VALUES ('40', 'nilai40', 'nilai40', 'nilai40', 'nilai40');
+INSERT INTO `datatable` VALUES ('41', 'nilai41', 'nilai41', 'nilai41', 'nilai41');
+INSERT INTO `datatable` VALUES ('42', 'nilai42', 'nilai42', 'nilai42', 'nilai42');
+INSERT INTO `datatable` VALUES ('43', 'nilai43', 'nilai43', 'nilai43', 'nilai43');
+INSERT INTO `datatable` VALUES ('44', 'nilai44', 'nilai44', 'nilai44', 'nilai44');
+INSERT INTO `datatable` VALUES ('45', 'nilai45', 'nilai45', 'nilai45', 'nilai45');
+INSERT INTO `datatable` VALUES ('46', 'nilai46', 'nilai46', 'nilai46', 'nilai46');
+INSERT INTO `datatable` VALUES ('47', 'nilai47', 'nilai47', 'nilai47', 'nilai47');
+INSERT INTO `datatable` VALUES ('48', 'nilai48', 'nilai48', 'nilai48', 'nilai48');
+INSERT INTO `datatable` VALUES ('49', 'nilai49', 'nilai49', 'nilai49', 'nilai49');
+INSERT INTO `datatable` VALUES ('50', 'nilai50', 'nilai50', 'nilai50', 'nilai50');
+INSERT INTO `datatable` VALUES ('51', 'nilai51', 'nilai51', 'nilai51', 'nilai51');
+INSERT INTO `datatable` VALUES ('52', 'nilai52', 'nilai52', 'nilai52', 'nilai52');
+INSERT INTO `datatable` VALUES ('53', 'nilai53', 'nilai53', 'nilai53', 'nilai53');
+INSERT INTO `datatable` VALUES ('54', 'nilai54', 'nilai54', 'nilai54', 'nilai54');
+INSERT INTO `datatable` VALUES ('55', 'nilai55', 'nilai55', 'nilai55', 'nilai55');
+INSERT INTO `datatable` VALUES ('56', 'nilai56', 'nilai56', 'nilai56', 'nilai56');
+INSERT INTO `datatable` VALUES ('57', 'nilai57', 'nilai57', 'nilai57', 'nilai57');
+INSERT INTO `datatable` VALUES ('58', 'nilai58', 'nilai58', 'nilai58', 'nilai58');
+INSERT INTO `datatable` VALUES ('59', 'nilai59', 'nilai59', 'nilai59', 'nilai59');
+INSERT INTO `datatable` VALUES ('60', 'nilai60', 'nilai60', 'nilai60', 'nilai60');
+INSERT INTO `datatable` VALUES ('61', 'nilai61', 'nilai61', 'nilai61', 'nilai61');
+INSERT INTO `datatable` VALUES ('62', 'nilai62', 'nilai62', 'nilai62', 'nilai62');
+INSERT INTO `datatable` VALUES ('63', 'nilai63', 'nilai63', 'nilai63', 'nilai63');
+INSERT INTO `datatable` VALUES ('64', 'nilai64', 'nilai64', 'nilai64', 'nilai64');
+INSERT INTO `datatable` VALUES ('65', 'nilai65', 'nilai65', 'nilai65', 'nilai65');
+INSERT INTO `datatable` VALUES ('66', 'nilai66', 'nilai66', 'nilai66', 'nilai66');
+INSERT INTO `datatable` VALUES ('67', 'nilai67', 'nilai67', 'nilai67', 'nilai67');
+INSERT INTO `datatable` VALUES ('68', 'nilai68', 'nilai68', 'nilai68', 'nilai68');
+INSERT INTO `datatable` VALUES ('69', 'nilai69', 'nilai69', 'nilai69', 'nilai69');
+INSERT INTO `datatable` VALUES ('70', 'nilai70', 'nilai70', 'nilai70', 'nilai70');
+INSERT INTO `datatable` VALUES ('71', 'nilai71', 'nilai71', 'nilai71', 'nilai71');
+INSERT INTO `datatable` VALUES ('72', 'nilai72', 'nilai72', 'nilai72', 'nilai72');
+INSERT INTO `datatable` VALUES ('73', 'nilai73', 'nilai73', 'nilai73', 'nilai73');
+INSERT INTO `datatable` VALUES ('74', 'nilai74', 'nilai74', 'nilai74', 'nilai74');
+INSERT INTO `datatable` VALUES ('75', 'nilai75', 'nilai75', 'nilai75', 'nilai75');
+INSERT INTO `datatable` VALUES ('76', 'nilai76', 'nilai76', 'nilai76', 'nilai76');
+INSERT INTO `datatable` VALUES ('77', 'nilai77', 'nilai77', 'nilai77', 'nilai77');
+INSERT INTO `datatable` VALUES ('78', 'nilai78', 'nilai78', 'nilai78', 'nilai78');
+INSERT INTO `datatable` VALUES ('79', 'nilai79', 'nilai79', 'nilai79', 'nilai79');
+INSERT INTO `datatable` VALUES ('80', 'nilai80', 'nilai80', 'nilai80', 'nilai80');
+INSERT INTO `datatable` VALUES ('81', 'nilai81', 'nilai81', 'nilai81', 'nilai81');
+INSERT INTO `datatable` VALUES ('82', 'nilai82', 'nilai82', 'nilai82', 'nilai82');
+INSERT INTO `datatable` VALUES ('83', 'nilai83', 'nilai83', 'nilai83', 'nilai83');
+INSERT INTO `datatable` VALUES ('84', 'nilai84', 'nilai84', 'nilai84', 'nilai84');
+INSERT INTO `datatable` VALUES ('85', 'nilai85', 'nilai85', 'nilai85', 'nilai85');
+INSERT INTO `datatable` VALUES ('86', 'nilai86', 'nilai86', 'nilai86', 'nilai86');
+INSERT INTO `datatable` VALUES ('87', 'nilai87', 'nilai87', 'nilai87', 'nilai87');
+INSERT INTO `datatable` VALUES ('88', 'nilai88', 'nilai88', 'nilai88', 'nilai88');
+INSERT INTO `datatable` VALUES ('89', 'nilai89', 'nilai89', 'nilai89', 'nilai89');
+INSERT INTO `datatable` VALUES ('90', 'nilai90', 'nilai90', 'nilai90', 'nilai90');
+INSERT INTO `datatable` VALUES ('91', 'nilai91', 'nilai91', 'nilai91', 'nilai91');
+INSERT INTO `datatable` VALUES ('92', 'nilai92', 'nilai92', 'nilai92', 'nilai92');
+INSERT INTO `datatable` VALUES ('93', 'nilai93', 'nilai93', 'nilai93', 'nilai93');
+INSERT INTO `datatable` VALUES ('94', 'nilai94', 'nilai94', 'nilai94', 'nilai94');
+INSERT INTO `datatable` VALUES ('95', 'nilai95', 'nilai95', 'nilai95', 'nilai95');
+INSERT INTO `datatable` VALUES ('96', 'nilai96', 'nilai96', 'nilai96', 'nilai96');
+INSERT INTO `datatable` VALUES ('97', 'nilai97', 'nilai97', 'nilai97', 'nilai97');
+INSERT INTO `datatable` VALUES ('98', 'nilai98', 'nilai98', 'nilai98', 'nilai98');
+INSERT INTO `datatable` VALUES ('99', 'nilai99', 'nilai99', 'nilai99', 'nilai99');
+INSERT INTO `datatable` VALUES ('100', 'nilai100', 'nilai100', 'nilai100', 'nilai100');
+INSERT INTO `datatable` VALUES ('101', 'nilai101', 'nilai101', 'nilai101', 'nilai101');
+INSERT INTO `datatable` VALUES ('102', 'nilai102', 'nilai102', 'nilai102', 'nilai102');
+INSERT INTO `datatable` VALUES ('103', 'nilai103', 'nilai103', 'nilai103', 'nilai103');
+INSERT INTO `datatable` VALUES ('104', 'nilai104', 'nilai104', 'nilai104', 'nilai104');
+INSERT INTO `datatable` VALUES ('105', 'nilai105', 'nilai105', 'nilai105', 'nilai105');
+INSERT INTO `datatable` VALUES ('106', 'nilai106', 'nilai106', 'nilai106', 'nilai106');
+INSERT INTO `datatable` VALUES ('107', 'nilai107', 'nilai107', 'nilai107', 'nilai107');
+INSERT INTO `datatable` VALUES ('108', 'nilai108', 'nilai108', 'nilai108', 'nilai108');
+INSERT INTO `datatable` VALUES ('109', 'nilai109', 'nilai109', 'nilai109', 'nilai109');
+INSERT INTO `datatable` VALUES ('110', 'nilai110', 'nilai110', 'nilai110', 'nilai110');
+INSERT INTO `datatable` VALUES ('111', 'nilai111', 'nilai111', 'nilai111', 'nilai111');
+INSERT INTO `datatable` VALUES ('112', 'nilai112', 'nilai112', 'nilai112', 'nilai112');
+INSERT INTO `datatable` VALUES ('113', 'nilai113', 'nilai113', 'nilai113', 'nilai113');
+INSERT INTO `datatable` VALUES ('114', 'nilai114', 'nilai114', 'nilai114', 'nilai114');
+INSERT INTO `datatable` VALUES ('115', 'nilai115', 'nilai115', 'nilai115', 'nilai115');
+INSERT INTO `datatable` VALUES ('116', 'nilai116', 'nilai116', 'nilai116', 'nilai116');
+INSERT INTO `datatable` VALUES ('117', 'nilai117', 'nilai117', 'nilai117', 'nilai117');
+INSERT INTO `datatable` VALUES ('118', 'nilai118', 'nilai118', 'nilai118', 'nilai118');
+INSERT INTO `datatable` VALUES ('119', 'nilai119', 'nilai119', 'nilai119', 'nilai119');
+INSERT INTO `datatable` VALUES ('120', 'nilai120', 'nilai120', 'nilai120', 'nilai120');
+INSERT INTO `datatable` VALUES ('121', 'gugus', 'gugus', 'gugus', 'gugus');
